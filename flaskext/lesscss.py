b431114f76a97027a4c7f8db254f74ec48bb80d4
sergej felde <sfelde@gmail.com>
2012-01-09 16:45:40 +0100

    rewrite lesscss

--- flaskext/lesscss.py
+++ flaskext/lesscss.py
@@ -17,32 +17,38 @@ def lesscss(app):
     def _render_less_css():
         if not hasattr(app, 'static_url_path'):
             from warnings import warn
+
             warn(DeprecationWarning('static_path is called '
                                     'static_url_path since Flask 0.7'),
-                                    stacklevel=2)
-        
+                stacklevel=2)
+
             static_url_path = app.static_path
-        
+
         else:
             static_url_path = app.static_url_path
-        
+
         static_dir = app.root_path + app.static_url_path
-        
+
         less_paths = []
         for path, subdirs, filenames in os.walk(static_dir):
             less_paths.extend([
-                os.path.join(path, f)
-                for f in filenames if os.path.splitext(f)[1] == '.less'
+            os.path.join(path, f)
+            for f in filenames if os.path.splitext(f)[1] == '.less'
             ])
-        
+
         for less_path in less_paths:
-            css_path = os.path.splitext(less_path)[0] + '.css'
+            filename = os.path.splitext(less_path)[0]
+            css_path = filename + '.css'
+            if not os.path.exists(css_path):
+                open(css_path, 'w')
             if not os.path.isfile(css_path):
                 css_mtime = -1
             else:
                 css_mtime = os.path.getmtime(css_path)
             less_mtime = os.path.getmtime(less_path)
             if less_mtime >= css_mtime:
-                subprocess.call(['lessc', less_path, css_path], shell=False)
+                #subprocess.call(['lessc', less_path, css_path], shell=False)
+                cmd = 'lessc ' + less_path + ' > ' + css_path
+                os.system(cmd)
 
 
